d520ad467bacd99b6510cae65a6280f31e46e570
Arnold <arnoldkorir201@gmail.com>
2025-06-16 18:30:45 +0300

    populate the pizza conroller to be able to view a list of pizzas

---
+++ server/controllers/pizza_controller.py
@@ -0,0 +1,10 @@
+# server/controllers/pizza_controller.py
+from flask import Blueprint, jsonify
+from server.models import Pizza
+
+pizzas_bp = Blueprint('pizzas', __name__)
+
+@pizzas_bp.route('/pizzas', methods=['GET'])
+def get_pizzas():
+    pizzas = Pizza.query.all()
+    return jsonify([pizza.to_dict() for pizza in pizzas])
