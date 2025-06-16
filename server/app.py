4a2652cc238719d7e561b4aa8bdf0b8b1dfe1055
Arnold <arnoldkorir201@gmail.com>
2025-06-16 18:34:55 +0300

    register the blue prints/routes

--- server/app.py
+++ server/app.py
@@ -13,14 +13,14 @@ def create_app(config_class=Config):
     db.init_app(app)
     migrate.init_app(app, db)
 
-    #  # Register blueprints (we'll add these later)
-    # from server.controllers.restaurant_controller import restaurants_bp
-    # from server.controllers.pizza_controller import pizzas_bp
-    # from server.controllers.restaurant_pizza_controller import restaurant_pizzas_bp
+    #register the Blueprints
+    from server.controllers.restaurant_controller import restaurants_bp
+    from server.controllers.pizza_controller import pizzas_bp
+    from server.controllers.restaurant_pizza_controller import restaurant_pizzas_bp
     
-    # app.register_blueprint(restaurants_bp)
-    # app.register_blueprint(pizzas_bp)
-    # app.register_blueprint(restaurant_pizzas_bp)
+    app.register_blueprint(restaurants_bp)
+    app.register_blueprint(pizzas_bp)
+    app.register_blueprint(restaurant_pizzas_bp)
 
 
     return app
