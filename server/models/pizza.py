ce36e998a46d2fb92cb13e2154b270d866844892
Arnold <arnoldkorir201@gmail.com>
2025-06-16 16:12:32 +0300

    add a custom method in pizza file that makes api response to json conversion easier and prevents exposure of metadata

--- server/models/pizza.py
+++ server/models/pizza.py
@@ -9,3 +9,14 @@ class Pizza(db.Model):
 
     #Relationship
     restaurant_pizzas = db.relationship('RestaurantPizza', backref='pizza')
+
+
+    def __repr__(self):
+        return f'<Pizza {self.name}>'
+    
+    def to_dict(self):
+        return {
+            'id':self.id,
+            'name':self.name,
+            'ingredients':self.ingredients
+        }
